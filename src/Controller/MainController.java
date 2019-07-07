830972bb308f1c3b75291d5885373799949fd178
anngo14 <31315148+anngo14@users.noreply.github.com>
2019-07-07 01:47:31 -0500

    Slight fade animation on Welcome View

--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -2,19 +2,25 @@ package Controller;
 
 import java.awt.Dimension;
 import java.awt.Font;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 
+import javafx.animation.FadeTransition;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
 
-public class MainController {
+public class MainController implements Initializable{
 
 	private static MainController instance = null;
 	
@@ -59,10 +65,6 @@ public class MainController {
 	{
 		
 	}
-	@FXML
-	public void initialize()
-	{
-	}
 	public int confirmationBox()
 	{
 		JFrame jf=new JFrame();
@@ -72,5 +74,17 @@ public class MainController {
 		UIManager.put("OptionPane.minimumSize",new Dimension(150,100)); 
 		int input = JOptionPane.showConfirmDialog(jf, label);
 		return input;
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		FadeTransition fade = new FadeTransition(Duration.millis(1450), panel);
+		fade.setFromValue(1.0);
+		fade.setToValue(0.95);
+		
+		fade.setCycleCount(Timeline.INDEFINITE);
+		fade.setAutoReverse(true);
+		fade.play();
+		
 	}
 }
