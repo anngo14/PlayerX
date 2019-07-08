61fad32c927ab9a5c2794736827e1a4a80ba104c
anngo14 <31315148+anngo14@users.noreply.github.com>
2019-07-08 05:19:12 -0500

    Implemented the DirectoryChoose and the first ListView

--- src/Controller/MusicController.java
+++ src/Controller/MusicController.java
@@ -1,25 +1,36 @@
 package Controller;
 
+import java.io.File;
 import java.net.URL;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.ResourceBundle;
 
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
 public class MusicController implements Initializable, Controller{
 
     private static DateTimeFormatter SHORT_TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm:ss");
-
+    HashMap<String, String> fileMap;
+    private final File defaultDir = new File("C:\\Users\\Andrew\\Music");
+	final DirectoryChooser dirChooser = new DirectoryChooser();
+	
 	@FXML
 	Label titleLabel;
 	@FXML
@@ -29,21 +40,56 @@ public class MusicController implements Initializable, Controller{
 	@FXML
 	Button fileButton;
 	@FXML
-	ListView list1;
+	ListView<String> list1;
 	@FXML
-	ListView list2;
+	ListView<String> list2;
 	@FXML
 	Button homeButton;
+	@FXML
+	StackPane panel;
 	
 	public MusicController()
 	{
-		
+		fileMap = new HashMap<String, String>();
 	}
 	
 	@FXML
 	public void uploadDir()
 	{
+		fileMap = new HashMap<String, String>();
+		Stage stage = (Stage) panel.getScene().getWindow();
+		File file = dirChooser.showDialog(stage);
 		
+		if(file != null)
+		{
+			pathTextField.setText(file.getAbsolutePath());
+		}
+		
+		listAllFiles(file.getAbsolutePath());
+		updateListView();
+		
+	}
+	public void listAllFiles(String path)
+	{
+		File folder = new File(path);
+		File[] files = folder.listFiles();
+		for (File f : files)
+	    {
+			if (f.isFile())
+	        {
+				fileMap.put(f.getName(), f.getAbsolutePath());
+	        }
+	        else if (f.isDirectory())
+	        {
+	            listAllFiles(f.getAbsolutePath());
+	        }
+	    }
+	}
+	public void updateListView()
+	{
+		list1.getItems().clear();
+		ObservableList<String> fLists = FXCollections.observableArrayList(fileMap.keySet());
+		list1.getItems().addAll(fLists);
 	}
 	@FXML
 	public void backToHome()
@@ -52,6 +98,9 @@ public class MusicController implements Initializable, Controller{
 	}
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
+		dirChooser.setInitialDirectory(defaultDir);
+		pathTextField.setText(defaultDir.getAbsolutePath());
+		listAllFiles(defaultDir.getAbsolutePath());
 		titleLabel.setText("Music");
 		Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(0),
 				event -> timeLabel.setText(LocalTime.now().format(SHORT_TIME_FORMATTER))),
@@ -59,6 +108,8 @@ public class MusicController implements Initializable, Controller{
 
 		timeline.setCycleCount(Animation.INDEFINITE);
 		timeline.play();
+		
+		updateListView();
 	}
 
 }
--- src/Controller/VideoController.java
+++ src/Controller/VideoController.java
@@ -1,14 +1,18 @@
 package Controller;
 
+import java.io.File;
 import java.net.URL;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
 import java.util.ResourceBundle;
 
 import javafx.animation.Animation;
 import javafx.animation.FadeTransition;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -17,13 +21,17 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
 public class VideoController implements Controller, Initializable{
 
 	FadeTransition fade;
     private static DateTimeFormatter SHORT_TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm:ss");
-
+    HashMap<String, String> fileMap;
+    private final File defaultDir = new File("C:\\Users\\Andrew\\Videos");
+	final DirectoryChooser dirChooser = new DirectoryChooser();
 	
 	@FXML
 	StackPane panel;
@@ -36,21 +44,53 @@ public class VideoController implements Controller, Initializable{
 	@FXML
 	Button fileButton;
 	@FXML
-	ListView list1;
+	ListView<String> list1;
 	@FXML
-	ListView list2;
+	ListView<String> list2;
 	@FXML
 	Button homeButton;
 	
 	public VideoController()
 	{
-		
+		fileMap = new HashMap<String, String>();
 	}
 	
 	@FXML
 	public void uploadDir()
 	{
+		fileMap = new HashMap<String, String>();
+		Stage stage = (Stage) panel.getScene().getWindow();
+		File file = dirChooser.showDialog(stage);
 		
+		if(file != null)
+		{
+			pathTextField.setText(file.getAbsolutePath());
+		}
+		listAllFiles(file.getAbsolutePath());
+		updateListView();
+
+	}
+	public void listAllFiles(String path)
+	{
+		File folder = new File(path);
+		File[] files = folder.listFiles();
+		for (File f : files)
+	    {
+			if (f.isFile())
+	        {
+				fileMap.put(f.getName(), f.getAbsolutePath());
+	        }
+	        else if (f.isDirectory())
+	        {
+	            listAllFiles(f.getAbsolutePath());
+	        }
+	    }
+	}
+	public void updateListView()
+	{
+		list1.getItems().clear();
+		ObservableList<String> fLists = FXCollections.observableArrayList(fileMap.keySet());
+		list1.getItems().addAll(fLists);
 	}
 	@FXML
 	public void backToHome()
@@ -59,6 +99,9 @@ public class VideoController implements Controller, Initializable{
 	}
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		dirChooser.setInitialDirectory(defaultDir);
+		pathTextField.setText(defaultDir.getAbsolutePath());
+		listAllFiles(defaultDir.getAbsolutePath());
 		titleLabel.setText("Video");	
 		Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(0),
 				event -> timeLabel.setText(LocalTime.now().format(SHORT_TIME_FORMATTER))),
@@ -66,6 +109,8 @@ public class VideoController implements Controller, Initializable{
 
 		timeline.setCycleCount(Animation.INDEFINITE);
 		timeline.play();
+		
+		updateListView();
 	}
 
 }
